c4d091d6fc0eed259e31be98240c8649d02950a1
MPyanykh <ronin25rw@gmail.com>
2024-12-18 14:36:27 +0200

    Add files via upload

---
+++ module_4_1.py
@@ -0,0 +1,11 @@
+from fake_math import divide as df
+from true_math import divide as dt
+
+result1 = df(15, 3)
+result2 = dt(96, 2)
+result3 = df(23, 0)
+result4 = dt(25, 0)
+print(result1)
+print(result2)
+print(result3)
+print(result4)
